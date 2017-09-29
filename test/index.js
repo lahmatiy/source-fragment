644f50648e9001db43b257345307ff1ffab1a71d
Roman Dvornov <rdvornov@gmail.com>
2017-09-29 18:41:03 +0300

    initial setup

---
+++ test/index.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var stub = require('../index.js');
+
+describe('stub test', function() {
+    it('should work', function() {
+        assert.equal(stub, true);
+    });
+});
